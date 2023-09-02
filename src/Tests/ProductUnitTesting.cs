429f906b256edbbf263eb2d19c1cdec2d3b0da08
matheusinit <matheus.oliveira.s@protonmail.com>
2023-09-01 22:53:27 -0300

    test: ensure when price is not provided then should throw

--- src/Tests/ProductUnitTesting.cs
+++ src/Tests/ProductUnitTesting.cs
@@ -2,12 +2,34 @@ namespace OrderingApi;
 
 using Xunit;
 
+/*
+ * TODO
+ *
+ * Properties:
+ *  - Name
+ *  - Price
+ *  - Description
+ *  - CreatedAt
+ *  - UpdatedAt
+ *  - DeletedAt
+ */
+
 public class ProductUnitTesting
 {
     [Fact]
     public void WhenNameIsNotProvidedShouldThrow()
     {
-        var exception = Record.Exception(() => new Product(_name: null));
+        var exception = Record.Exception(() => new Product(_name: null, _price: 0));
+
+        Assert.IsType<ArgumentException>(exception);
+    }
+
+    [Fact]
+    public void WhenPriceIsNotProvidedThenShouldThrow()
+    {
+        var exception = Record.Exception(
+            () => new Product(_name: "Rustic Frozen Pizza", _price: null)
+        );
 
         Assert.IsType<ArgumentException>(exception);
     }
