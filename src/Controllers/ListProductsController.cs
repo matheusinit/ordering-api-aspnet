30761b28abfd4c5a06dbebf2f702aab4eea5a4b5
matheusinit <matheus.oliveira.s@protonmail.com>
2023-09-29 08:15:00 -0300

    feat: ensure when no products exist then should get empty list

---
+++ src/Controllers/ListProductsController.cs
@@ -0,0 +1,21 @@
+namespace OrderingApi.Controllers;
+
+using System.Collections;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("/products")]
+public class ListProductsController : ControllerBase
+{
+    private readonly ILogger<ListProductsController> _logger;
+
+    public ListProductsController(ILogger<ListProductsController> logger)
+    {
+        _logger = logger;
+    }
+
+    public ActionResult<HttpResponse> List()
+    {
+        return Ok(new ArrayList());
+    }
+}
