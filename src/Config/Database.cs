477a0408be4ef0b62510c53a3dbc90b5987c3dc1
matheusinit <matheus.oliveira.s@protonmail.com>
2023-10-02 08:32:37 -0300

    refactor: return raw connection string when is in development mode

--- src/Config/Database.cs
+++ src/Config/Database.cs
@@ -4,7 +4,13 @@ public static class DatabaseConnection
 {
     public static string GetConnectionString()
     {
-        if (Env.DB_HOST == null)
+        var isDevelopment = string.Equals(
+            Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"),
+            "development",
+            StringComparison.InvariantCultureIgnoreCase
+        );
+
+        if (isDevelopment)
             return "Server=192.168.0.18,1433;User Id=sa;Database=master;Trusted_Connection=false;Password=PandaNinja13.;TrustServerCertificate=true;";
 
         return $"Server={Env.DB_HOST},{Env.DB_PORT};User Id={Env.DB_USER};Database={Env.DB_NAME};Trusted_Connection=false;Password={Env.DB_PASSWORD};TrustServerCertificate=true;";
