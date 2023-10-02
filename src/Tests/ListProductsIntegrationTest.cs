bb6c8be373978bf27f698e3824020606fb9c95a6
matheusinit <matheus.oliveira.s@protonmail.com>
2023-10-02 15:06:24 -0300

    test: ensure when there is products stored then should get product price in double

--- src/Tests/ListProductsIntegrationTest.cs
+++ src/Tests/ListProductsIntegrationTest.cs
@@ -9,9 +9,9 @@ using Microsoft.EntityFrameworkCore;
 
 class Product
 {
-    public string id { get; set; }
-    public string name { get; set; }
-    public int price { get; set; }
+    public required string id { get; set; }
+    public required string name { get; set; }
+    public double price { get; set; }
     public string? description { get; set; }
     public DateTime createdAt { get; set; }
     public DateTime? updatedAt { get; set; }
@@ -54,8 +54,26 @@ public class ListProductsIntegrationTest : IClassFixture<WebApplicationFactory<P
 
         var list = await _client.GetFromJsonAsync<List<Product>>("/products");
 
+        var productPriceInDouble = product?.price / 100;
         Assert.NotEmpty(list);
-        Assert.Equal(product.name, list[0].name);
-        Assert.Equal(product.price, list[0].price);
+        Assert.Equal(product?.name, list[0].name);
+        Assert.Equal(productPriceInDouble, list[0].price);
+    }
+
+    [Fact]
+    public async Task WhenThereIsProductsStoredThenShouldGetProductPriceInDouble()
+    {
+        var randomProductName = new Faker().Commerce.ProductName();
+        var randomPrice = new Faker().Random.Int(0, 999999);
+        var response = await _client.PostAsJsonAsync(
+            "/products",
+            new { name = randomProductName, price = randomPrice }
+        );
+        var product = await response.Content.ReadFromJsonAsync<Product>();
+
+        var list = await _client.GetFromJsonAsync<List<Product>>("/products");
+
+        var productPriceInDouble = product?.price / 100;
+        Assert.Equal(productPriceInDouble, list[0].price);
     }
 }
