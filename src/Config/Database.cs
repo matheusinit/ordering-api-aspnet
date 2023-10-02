47fa160ad6be003585b5345086a294983028bb2f
matheusinit <matheus.oliveira.s@protonmail.com>
2023-10-01 22:59:26 -0300

    fix: define connection string when using ef core cli because I dont know how to load .env with cli commands

--- src/Config/Database.cs
+++ src/Config/Database.cs
@@ -1,9 +1,12 @@
 namespace OrderingApi.Config;
 
-public class DatabaseConnection
+public static class DatabaseConnection
 {
     public static string GetConnectionString()
     {
+        if (Env.DB_HOST == null)
+            return "Server=192.168.0.18,1433;User Id=sa;Database=master;Trusted_Connection=false;Password=PandaNinja13.;TrustServerCertificate=true;";
+
         return $"Server={Env.DB_HOST},{Env.DB_PORT};User Id={Env.DB_USER};Database={Env.DB_NAME};Trusted_Connection=false;Password={Env.DB_PASSWORD};TrustServerCertificate=true;";
     }
 }
