feb8144e7176392f876b8820888163b8ce6a7e0b
matheusinit <matheus.oliveira.s@protonmail.com>
2023-10-03 18:30:27 -0300

    test: ensure when valid id is provided then should delete product from database

--- src/Tests/DeleteProductIntegrationTest.cs
+++ src/Tests/DeleteProductIntegrationTest.cs
@@ -2,6 +2,7 @@ using System.Net;
 using Bogus;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
+using OrderingApi.Data;
 using Xunit;
 
 namespace OrderingApi.IntegrationTest;
@@ -49,5 +50,28 @@ public class DeleteProductIntegrationTesting : IClassFixture<WebApplicationFacto
         var response = await _client.DeleteAsync($"/products/{id}");
 
         Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task WhenValidIdIsProvidedThenShouldDeleteProductFromDatabase()
+    {
+        var randomProductName = new Faker().Commerce.ProductName();
+        var randomPrice = new Faker().Random.Int(0, 999999);
+        var randomDescription = new Faker().Lorem.Sentence();
+        var insertionData = new
+        {
+            name = randomProductName,
+            price = randomPrice,
+            description = randomDescription
+        };
+        var responseCreation = await _client.PostAsJsonAsync("/products", insertionData);
+        var responseBodyCreation = await responseCreation.Content.ReadFromJsonAsync<Product>();
+        var id = responseBodyCreation?.id;
+        var response = await _client.DeleteAsync($"/products/{id}");
+        var context = new ApplicationContext();
+
+        var product = await context.Products.FindAsync(id);
+
+        Assert.Null(product);
     }
 }
