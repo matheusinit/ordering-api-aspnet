9faddca3a993f6d1cd07a06fd6b1bf217133d44b
matheusinit <matheus.oliveira.s@protonmail.com>
2023-10-03 18:20:54 -0300

    feat: ensure when valid id is provided then should get no content

--- src/Controllers/DeleteProductController.cs
+++ src/Controllers/DeleteProductController.cs
@@ -1,21 +1,34 @@
 namespace OrderingApi.Controllers;
 
 using Microsoft.AspNetCore.Mvc;
+using OrderingApi.Data;
 
 [ApiController]
 [Route("/products")]
 public class DeleteProductController : ControllerBase
 {
     private readonly ILogger<DeleteProductController> _logger;
+    private readonly ApplicationContext _context;
 
-    public DeleteProductController(ILogger<DeleteProductController> logger)
+    public DeleteProductController(
+        ILogger<DeleteProductController> logger,
+        ApplicationContext context
+    )
     {
         _logger = logger;
+        _context = context;
     }
 
     [HttpDelete("{id}")]
     public ActionResult<HttpResponse> Delete(string id)
     {
-        return NotFound(new { message = "Product not found" });
+        var productFound = _context.Products.Find(id);
+
+        if (productFound == null)
+        {
+            return NotFound(new { message = "Product not found" });
+        }
+
+        return NoContent();
     }
 }
