86f6ba66dbf94e92fac4d215974355d8778bc725
matheusinit <matheus.oliveira.s@protonmail.com>
2023-10-06 23:47:20 -0300

    feat: when instantiated with product then should assign an id

--- src/Domain/Order.cs
+++ src/Domain/Order.cs
@@ -3,10 +3,18 @@ namespace OrderingApi.Domain;
 public class Order
 {
     private Product product;
+    private string id;
 
     public Order(Product _product)
     {
         Product = product = _product;
+        Id = Guid.NewGuid().ToString();
+    }
+
+    public string Id
+    {
+        get => id;
+        set { id = value; }
     }
 
     public Product Product
