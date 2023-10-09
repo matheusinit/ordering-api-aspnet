ff31d12f6ed3f9d86ec28f79270d135f1d33c055
matheusinit <matheus.oliveira.s@protonmail.com>
2023-10-08 21:17:50 -0300

    test: ensure when product id exists then should get ok

--- src/Tests/OrderProdutIntegrationTest.cs
+++ src/Tests/OrderProdutIntegrationTest.cs
@@ -1,21 +1,35 @@
+namespace OrderingApi.IntegrationTest;
+
 using System.Net;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
+using OrderingApi.Data;
+using OrderingApi.Domain;
 using Xunit;
 
-namespace OrderingApi.IntegrationTest;
+public class OrderProductResponseBody
+{
+    public string id { get; set; }
+    public string status { get; set; }
+    public string productId { get; set; }
+    public string createdAt { get; set; }
+    public string updatedAt { get; set; }
+    public string deletedAt { get; set; }
+}
 
 [Collection("Sequential")]
 public class OrderProductIntegrationTest : IClassFixture<WebApplicationFactory<Program>>
 {
     private WebApplicationFactory<Program> _factory = new WebApplicationFactory<Program>();
     private HttpClient _client;
+    private readonly ApplicationContext _context;
 
     public OrderProductIntegrationTest()
     {
         _client = _factory
             .WithWebHostBuilder(builder => builder.UseSolutionRelativeContentRoot(".."))
             .CreateClient();
+        _context = new ApplicationContext();
     }
 
     [Fact]
@@ -39,5 +53,24 @@ public class OrderProductIntegrationTest : IClassFixture<WebApplicationFactory<P
         var responseBody = await response.Content.ReadFromJsonAsync<ResponseError>();
         Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
         Assert.Equal(responseBody?.message, "Product not found");
+    }
+
+    [Fact]
+    public async Task WhenProductIdExistsThenShouldGetOk()
+    {
+        var product = new Product(
+            _name: "Product 1",
+            _price: 100,
+            _description: "Description 1",
+            _id: Guid.NewGuid().ToString()
+        );
+        _context.Products.Add(product);
+        await _context.SaveChangesAsync();
+
+        var response = await _client.PostAsJsonAsync("/order", new { productId = product.Id });
+
+        var responseBody = await response.Content.ReadFromJsonAsync<OrderProductResponseBody>();
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        Assert.Equal(product.Id, responseBody?.productId);
     }
 }
