28c9778b12222810cd89e9b9926e3d62ba67231c
matheusinit <matheus.oliveira.s@protonmail.com>
2023-10-08 21:08:39 -0300

    feat: ensure when product id does not exist then should get not found

--- src/Controllers/OrderProductController.cs
+++ src/Controllers/OrderProductController.cs
@@ -1,13 +1,38 @@
 namespace OrderingApi.Controllers;
 
 using Microsoft.AspNetCore.Mvc;
+using OrderingApi.Data;
+
+public class OrderProductRequest
+{
+    public string? productId { get; set; }
+}
 
 [ApiController]
 [Route("/order")]
 public class OrderProductController : ControllerBase
 {
-    public ActionResult<HttpResponse> Order()
+    private readonly ApplicationContext _context;
+
+    public OrderProductController(ApplicationContext context)
     {
-        return BadRequest(error: new { message = "Product id is required" });
+        _context = context;
+    }
+
+    public ActionResult<HttpResponse> Order([FromBody] OrderProductRequest order)
+    {
+        var product = _context.Products.Find(order.productId);
+
+        if (order.productId == null)
+        {
+            return BadRequest(error: new { message = "Product id is required" });
+        }
+
+        if (product == null)
+        {
+            return NotFound(new { message = "Product not found" });
+        }
+
+        return Ok();
     }
 }
