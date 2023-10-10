10470f114ddea044fbed043960384d3fad0221a9
matheusinit <matheus.oliveira.s@protonmail.com>
2023-10-09 21:01:43 -0300

    feat: ensure when there is orders stored then should get list with orders

--- src/Controllers/ListOrdersController.cs
+++ src/Controllers/ListOrdersController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using Microsoft.AspNetCore.Mvc;
+using OrderingApi.Data;
 
 namespace OrderingApi.Controllers;
 
@@ -7,9 +8,31 @@ namespace OrderingApi.Controllers;
 [Route("/orders")]
 public class ListOrdersController : ControllerBase
 {
+    private readonly ApplicationContext _context;
+
+    public ListOrdersController(ApplicationContext context)
+    {
+        _context = context;
+    }
+
     [HttpGet]
     public ActionResult<HttpResponse> List()
     {
-        return Ok(new ArrayList());
+        var orders = _context.Orders.ToList();
+
+        var view = orders.Select(o =>
+        {
+            return new OrderView
+            {
+                id = o.Id,
+                status = "Not sent",
+                productId = o.ProductId,
+                createdAt = o.CreatedAt,
+                updatedAt = o.UpdatedAt,
+                cancelAt = o.CanceledAt
+            };
+        });
+
+        return Ok(view);
     }
 }
