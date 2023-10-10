159ce4d1e07824a3b90234c888eb40fbad80d6e3
matheusinit <matheus.oliveira.s@protonmail.com>
2023-10-09 21:01:54 -0300

    test: ensure when there is orders stored then should get list with orders

--- src/Tests/ListOrdersIntegrationTest.cs
+++ src/Tests/ListOrdersIntegrationTest.cs
@@ -3,6 +3,9 @@ namespace OrderingApi.IntegrationTest;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using OrderingApi.Data;
+using OrderingApi.Domain;
 using Xunit;
 
 public class ListOrdersIntegrationTest : IClassFixture<WebApplicationFactory<Program>>
@@ -20,9 +23,35 @@ public class ListOrdersIntegrationTest : IClassFixture<WebApplicationFactory<Pro
     [Fact]
     public async Task WhenNoOrdersExistThenShouldGetEmptyList()
     {
+        var context = new ApplicationContext();
+
+        context.Orders.ExecuteDelete<Order>();
+
         var response = await _client.GetAsync("/orders");
         var list = await response.Content.ReadFromJsonAsync<List<OrderView>>();
 
         list.Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task WhenThereIsOrdersStoredThenShouldGetListWithOrders()
+    {
+        var context = new ApplicationContext();
+        var product = new Product
+        {
+            Id = "1",
+            Name = "Product 1",
+            Price = 100
+        };
+        context.Products.Add(product);
+
+        var order = new Domain.Order(_product: product);
+        context.Orders.Add(order);
+        await context.SaveChangesAsync();
+
+        var response = await _client.GetAsync("/orders");
+        var list = await response.Content.ReadFromJsonAsync<List<OrderView>>();
+
+        list.Should().NotBeEmpty();
     }
 }
