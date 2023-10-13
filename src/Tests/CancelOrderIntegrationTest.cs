71ce25d6e5778636e4bfef30a875f3b7ea0a263b
matheusinit <matheus.oliveira.s@protonmail.com>
2023-10-13 08:36:59 -0300

    test: ensure when id of canceled order is provided then should get bad request

--- src/Tests/CancelOrderIntegrationTest.cs
+++ src/Tests/CancelOrderIntegrationTest.cs
@@ -3,18 +3,23 @@ namespace OrderingApi.IntegrationTest;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
+using OrderingApi.Data;
+using OrderingApi.Domain;
 using Xunit;
 
 public class CancelOrderIntegrationTest : IClassFixture<WebApplicationFactory<Program>>
 {
     private WebApplicationFactory<Program> _factory = new WebApplicationFactory<Program>();
     private HttpClient _client;
+    private ApplicationContext _context;
 
     public CancelOrderIntegrationTest()
     {
         _client = _factory
             .WithWebHostBuilder(builder => builder.UseSolutionRelativeContentRoot(".."))
             .CreateClient();
+
+        _context = new ApplicationContext();
     }
 
     [Fact]
@@ -37,5 +42,26 @@ public class CancelOrderIntegrationTest : IClassFixture<WebApplicationFactory<Pr
         httpResponse.StatusCode
             .Should()
             .Be((System.Net.HttpStatusCode)StatusCodes.Status404NotFound);
+    }
+
+    [Fact]
+    public async Task WhenIdOfCanceledOrderIsProvidedThenShouldGetBadRequest()
+    {
+        var product = new Product(_name: "Product 1", _price: 100, _description: "Description 1");
+        var order = new Order(product);
+        order.Cancel();
+
+        _context.Products.Add(product);
+        _context.Orders.Add(order);
+        await _context.SaveChangesAsync();
+
+        var id = order.Id;
+        var httpResponse = await _client.PatchAsync($"/orders/{id}", null);
+        var responseBody = await httpResponse.Content.ReadFromJsonAsync<ResponseError>();
+
+        httpResponse.StatusCode
+            .Should()
+            .Be((System.Net.HttpStatusCode)StatusCodes.Status400BadRequest);
+        responseBody?.message.Should().Be("Order is already canceled");
     }
 }
