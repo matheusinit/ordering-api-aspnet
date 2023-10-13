e8ac19265a70cced71e24393f07d6898080e61c7
matheusinit <matheus.oliveira.s@protonmail.com>
2023-10-12 22:14:14 -0300

    test: ensure when invalid id is provided then should get bad request

---
+++ src/Tests/CancelOrderIntegrationTest.cs
@@ -0,0 +1,30 @@
+namespace OrderingApi.IntegrationTest;
+
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Xunit;
+
+public class CancelOrderIntegrationTest : IClassFixture<WebApplicationFactory<Program>>
+{
+    private WebApplicationFactory<Program> _factory = new WebApplicationFactory<Program>();
+    private HttpClient _client;
+
+    public CancelOrderIntegrationTest()
+    {
+        _client = _factory
+            .WithWebHostBuilder(builder => builder.UseSolutionRelativeContentRoot(".."))
+            .CreateClient();
+    }
+
+    [Fact]
+    public async Task WhenInvalidIdIsProvidedThenShouldGetBadRequest()
+    {
+        var invalidId = 1;
+        var httpResponse = await _client.PatchAsync($"/orders/{invalidId}", null);
+
+        httpResponse.StatusCode
+            .Should()
+            .Be((System.Net.HttpStatusCode)StatusCodes.Status400BadRequest);
+    }
+}
