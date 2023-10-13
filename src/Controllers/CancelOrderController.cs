d0d704a6355b38d05972cee668af7345160c935b
matheusinit <matheus.oliveira.s@protonmail.com>
2023-10-13 08:56:37 -0300

    feat: ensure when valid id is provided then should get ok

--- src/Controllers/CancelOrderController.cs
+++ src/Controllers/CancelOrderController.cs
@@ -36,6 +36,17 @@ public class CancelOrderController : ControllerBase
             return BadRequest(error: new { message = "Order is already canceled" });
         }
 
-        return BadRequest();
+        var view = new OrderView();
+
+        view.setValues(
+            _id: order.Id,
+            _status: order.Status,
+            _productId: order.ProductId,
+            _createdAt: order.CreatedAt,
+            _updatedAt: order.UpdatedAt,
+            _canceledAt: order.CanceledAt
+        );
+
+        return Ok(view);
     }
 }
